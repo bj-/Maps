b2594b802978eab2f62b16c6feae69d76452646f
bj- <bmw518@bk.ru>
2018-07-20 21:31:09 +0300

    create all

--- POI/kml2gpx.cmd
+++
@@ -1,7 +0,0 @@
-SET GPSBAB=C:\Program Files (x86)\GPSBabel\gpsbabel.exe
-SET M_SOURCE=D:/GitHubRep/Maps/POI
-SET M_TARGET=D:/GitHubRep/Maps/POI
-
-rem "%GPSBAB%" -w -r -t -i kml -f "%M_SOURCE%/RU-POI.kml" -o gpx -F "%M_SOURCE%/RU-POI.gpx"
-
-for %%f in (*.kml) do "%GPSBAB%" -w -r -t -i kml -f "%M_SOURCE%/%%~nf.kml" -o gpx -F "%M_TARGET%/%%~nf.gpx"
--- kml2gpx.cmd
+++ kml2gpx.cmd
@@ -1,9 +1,7 @@
 SET GPSBAB=C:\Program Files (x86)\GPSBabel\gpsbabel.exe
-SET M_SOURCE=D:/GitHubRep/Maps
-SET M_TARGET=D:/GitHubRep/Maps
+SET M_SOURCE=D:\_Disk\_GoogleDrive\Maps
+SET M_TARGET=D:\_Disk\_GoogleDrive\Maps
 
 rem "%GPSBAB%" -w -r -t -i kml -f "%M_SOURCE%/RU-POI.kml" -o gpx -F "%M_SOURCE%/RU-POI.gpx"
 
 for %%f in (*.kml) do "%GPSBAB%" -w -r -t -i kml -f "%M_SOURCE%/%%~nf.kml" -o gpx -F "%M_TARGET%/%%~nf.gpx"
-
-pause
